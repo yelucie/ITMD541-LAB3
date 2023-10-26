440eee49349ac0236a60c214caf0a1cdcacd0031
yelucie <lucie.ye@reseau.eseo.fr>
2023-10-26 17:10:15 -0500

    HTML & JS

---
+++ calculator.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", (event)=>{
+    // User's input
+    var bill = document.getElementById("bill");
+    var tip = document.getElementById("tip");
+
+    // Error management
+    var error = document.createElement('p');
+    error.id = 'error';
+    let errorMsg = document.createTextNode('Please enter a 2 decimal number as follow: 12.34');
+    error.appendChild(errorMsg);
+
+    var billVal = 0;
+    var tipVal = 15;
+
+    bill.addEventListener("input", calculator);
+    tip.addEventListener("input", calculator);
+
+    function calculator(){
+        if (isNaN(Number(bill.value))){
+            if(document.getElementById('error') === null){
+                document.getElementsByClassName('calculator')[0].appendChild(error);
+            }
+        } else if (Number(bill.value).toFixed(2) != Number(bill.value)) {
+            document.getElementsByClassName('calculator')[0].appendChild(error);
+        } else {
+            if (error !== null) error.remove();
+
+            billVal = Number(bill.value).toFixed(2) || 0;
+            tipVal = Number(tip.value) || 0;
+
+            // Setting the Tip Percentage field
+            var tipPercentageInput = document.getElementById("tip-percentage");
+            tipPercentageInput.value = tipVal;
+
+            // Setting the Tip Amount field
+            var tipAmount = document.getElementById("tip-amount");
+            var tipAmountValue = tipVal / 100 * billVal;
+            tipAmount.value = tipAmountValue.toFixed(2);
+
+            // Setting the Total Bill with Tip field
+            var total = document.getElementById("total");
+            var totalValue = Number(billVal) + Number(tipAmountValue);
+            total.value = totalValue.toFixed(2);
+        }
+    }
+});
